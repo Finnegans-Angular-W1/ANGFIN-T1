af280157856f68b9a8051454b2fc799ced6c4e4f
Lucas Alegre <lucasealegre123@gmail.com>
2023-02-01 10:31:47 -0300

    Componente 404 added

--- src/app/pages/page-not-found/page-not-found-routing.module.ts
+++ src/app/pages/page-not-found/page-not-found-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class PageNotFoundRoutingModule { }
