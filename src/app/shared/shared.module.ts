4b3c13f1174b7fa998ecc66ced3ca05708359df6
MaxiTRR <maxihtorr@gmail.com>
2023-02-01 19:59:18 -0300

    Creacion del dialog de Term. y condiciones

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
-
+import { DialogComponent } from './components/dialog/dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DialogComponent
+  ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MatDialogModule
+    
   ]
 })
 export class SharedModule { }
