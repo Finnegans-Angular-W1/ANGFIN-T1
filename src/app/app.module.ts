80dd9a48d5f12246156bdd380c0fc102f39e25b1
PiterPiojo <delgado.aldo87@gmail.com>
2023-02-02 10:31:12 -0300

    Agregada libreria nrgx, Agreado el Action, reducers y models.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { ROOT_REDUCERS } from './core/state/app.reducer';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot(ROOT_REDUCERS),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/models/auth.ts
@@ -0,0 +1,25 @@
+import { User } from "./user";
+
+export interface AuthState {
+    user: User |null;
+    isAuthenticated: boolean;
+    isLoading: boolean | null;
+    accessToken: string | null;
+    error:string | null;
+}
+
+export interface LoginInput {
+    email: string;
+    password: string;
+}
+
+export interface LoginResult {
+    accessToken: string;
+}
+
+export interface RegisterInput {
+    first_name?: string;
+    last_name?: string;
+    email: string;
+    password: string;
+}
