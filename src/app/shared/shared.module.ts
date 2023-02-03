ed97f39671c09dd9fec75fd21fb823dfbed72bfc
MaxiTRR <maxihtorr@gmail.com>
2023-02-03 16:10:56 -0300

    fix despues de guardar cambios PR Conflict 2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,13 +8,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { SharedRoutingModule } from './shared-routing.module';
-<<<<<<< HEAD
 import { FooterComponent } from '../components/footer/footer.component';
 
-=======
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { LoaderComponent } from './components/loader/loader.component';
->>>>>>> 5ec112466083afc1f4885f9db8833989b4e40c9e
 import { DialogComponent } from './components/dialog/dialog.component';
 import {MatDialogModule} from '@angular/material/dialog';
 
@@ -22,11 +19,8 @@ import {MatDialogModule} from '@angular/material/dialog';
 @NgModule({
   declarations: [
     SidebarComponent,
-<<<<<<< HEAD
     FooterComponent,
-=======
     LoaderComponent,
->>>>>>> 5ec112466083afc1f4885f9db8833989b4e40c9e
     DialogComponent,
   ],
   imports: [
