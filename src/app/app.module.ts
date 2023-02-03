09fe820759d49ef67c325a1d14c45bc88f973354
Adrian Latorre <adrian.latorre@gmail.com>
2023-02-03 03:02:46 -0300

    Maquetado HOME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RtitleComponent
+    RtitleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
