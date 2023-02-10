ad3b2f3e44ac785827c176e5301027aedb1797d8
MaxiTRR <maxihtorr@gmail.com>
2023-02-09 22:24:36 -0300

    codigo comentado de obtencion de datos de la API. Fixear.

--- src/app/shared/components/exchange-rate/exchange-rate.component.ts
+++ src/app/shared/components/exchange-rate/exchange-rate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExchangeService } from 'src/app/core/services/exchange.service';
 
 @Component({
   selector: 'app-exchange-rate',
@@ -6,13 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exchange-rate.component.scss']
 })
 export class ExchangeRateComponent implements OnInit {
+  public valorCompra:string = '';
+  public valorVenta:string = '';
   
   // Ttile que iba en el componente rtitle, probablemente copiar al componente contenedor
   //exchangeTitle:string = 'Tipo de cambio del dia de la fecha';  
 
-  constructor() { }
+  constructor(private excSvc:ExchangeService) { }
 
   ngOnInit(): void {
+    //this.getValores()
   }
 
-}
+  
+  //Metodo para obtener los valores del Api de cotizacion de Dolar. Comentado ya que todavia
+  //no se aprobo el PR con el servcie de exchange actualizado con la API. REVISAR CODIGO
+  
+  /*getValores(){
+    this.excSvc.getDolar().subscribe(
+      res =>{
+        this.valorCompra = res[0].casa.compra;
+        this.valorVenta = res[0].casa.venta;
+      }
+    );*/
+  }
+
