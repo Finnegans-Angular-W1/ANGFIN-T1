c49c176e240e90c3927309730d43c4ef3e381df7
MaxiTRR <maxihtorr@gmail.com>
2023-02-10 15:50:01 -0300

    Creacion del comp. container de exchange(divisas).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import { AuthRegistroRoutingModule } from './pages/auth-registro/auth-registro-r
 import { AuthRegistroModule } from './pages/auth-registro/auth-registro.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ROOT_REDUCERS } from './core/state/app.state';
+import { ExchangeContainerComponent } from './pages/home/components/exchange-container/exchange-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ExchangeContainerComponent,
   ],
   imports: [
     BrowserModule,
