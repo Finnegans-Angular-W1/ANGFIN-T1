bf08fb964f84625d0154fb582e88368fa5b05100
Uriel Olmedo <uriel.olme@gmail.com>
2023-02-15 15:22:54 -0300

    implemetacion de get register

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { LoginInput, LoginResult } from '../models/auth';
+import { LoginInput, LoginResult, RegisterInput } from '../models/auth';
 import { HttpService } from './http.service';
 import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+import { User } from '../models/user';
+import { ToastService } from 'angular-toastify';
 
 
 
@@ -14,16 +15,16 @@ import { Router } from '@angular/router';
 export class AuthService {
   constructor(
     private htpp: HttpService,
-    private router: Router
-
+    private router: Router,
+    private toast: ToastService
   ) {}
 
-  saveToken(token: string|null) {
+  saveToken(token: string | null) {
     if (token) sessionStorage.setItem('accessToken', token);
   }
 
-  getToken() {    
-    return sessionStorage.getItem('accessToken')
+  getToken() {
+    return sessionStorage.getItem('accessToken');
   }
 
   logOut() {
@@ -32,17 +33,34 @@ export class AuthService {
   }
 
   logIn(loginInput: LoginInput): Observable<LoginResult> {
-    return this.htpp
-      .post<LoginResult>(`/auth/login`, loginInput)
-      .pipe(
-        map((res: LoginResult) => {
-          this.router.navigate(['home']);
-          return res;
-        }),
-        catchError(err => {
-         return this.handleLoginError(err)       
-        })
-      );
+    return this.htpp.post<LoginResult>(`/auth/login`, loginInput).pipe(
+      map((res: LoginResult) => {
+        this.router.navigate(['home']);
+        return res;
+      }),
+      catchError(err => {
+        return this.handleLoginError(err);
+      })
+    );
+  }
+
+  register(registerInput: RegisterInput): Observable<any> {
+    return this.htpp.post<User>('/users', registerInput).pipe(
+      map((res: User) => {
+        return res;
+      }),
+      catchError(err => {
+        return this.handleRegisterError(err);
+      })
+    );
+  }
+
+  private handleRegisterError(err: HttpErrorResponse): Observable<never> {
+    console.log(err.error.error);
+    if (err.error.error.split(' ')[0] === 'Duplicate') {
+      return throwError(() => new Error('El correo ingresado ya esta registrado'));
+    }
+    return throwError(() => new Error('Ha ocurrido un error al intentar crear el usuario'));
   }
 
   private handleLoginError(err: HttpErrorResponse): Observable<never> {
